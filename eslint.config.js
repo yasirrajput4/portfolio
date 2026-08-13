e720ef02d24768efb6c690ab075a8ae8d7818050
Yasir Rajput <rajputyasir2005@gmail.com>
2026-08-13 22:34:45 +0530

    fix: resolve inconsistent ecmaVersion in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ export default [
   {
     files: ["**/*.{js,jsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: "latest",
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: "latest",
